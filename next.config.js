0c23aca976b17852da5ac8b497668d4e34a6c34a
HaseebAhmad <haseebahmed50411@gmail.com>
2024-09-09 22:30:39 +0500

    dependencies update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Disable specific ESLint rules
+    // Disable ESLint warnings during builds
     ignoreDuringBuilds: true,
-    rules: {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off",
-    },
   },
 };
 
 module.exports = nextConfig;
+
